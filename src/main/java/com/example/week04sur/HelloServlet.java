a6aa5ddfc95bdb4520b45e839a5616c9d01b6ab8
LuongMinhNhat <114485513+kusocisme@users.noreply.github.com>
2023-04-05 21:17:23 +0700

    Update HelloServlet.java

--- src/main/java/com/example/week04sur/HelloServlet.java
+++ src/main/java/com/example/week04sur/HelloServlet.java
@@ -5,6 +5,7 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+// iu kusocisme
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
